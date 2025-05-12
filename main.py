81aac0b8eed69ea054fd0ea3ca7ec1d2bd42790b
gnemek20 <gnemek20@gmail.com>
2025-05-13 00:12:46 +0900

    최적의 조합을 산출해내도록 변경

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post("/help-core/")
-async def help(files: list[UploadFile] = File(...), job: str = Form(...)):
+async def help(files: list[UploadFile] = File(...), job: str = Form(...), target_skills: list[str] = Form(...)):
   try:
     displays = []
     for file in files:
@@ -46,10 +46,12 @@ async def help(files: list[UploadFile] = File(...), job: str = Form(...)):
       core_skills = get_skills(cores)
       parsed_skills = parse_skills(core_skills, job_dir)
 
-      _, skill_names = zip(*parsed_skills)
-      target_skills = ["레조네이트", "데스 블로섬"]
-      combinations = find_combinations(skill_names, target_skills)
+      if len(parsed_skills) < 1:
+        continue
 
+      _, skill_names = zip(*parsed_skills)
+      combinations = find_combinations(skill_names, target_skills)
+      print(combinations)
       results.append(combinations)
 
     return JSONResponse(content={
@@ -67,136 +69,260 @@ async def help(files: list[UploadFile] = File(...), job: str = Form(...)):
 def get_table(display, template):
   gray_display = cv2.cvtColor(display, cv2.COLOR_BGR2GRAY)
   gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+  # 테이블 템플릿 매칭
   result = cv2.matchTemplate(gray_display, gray_template, cv2.TM_CCOEFF_NORMED)
   _, _, _, max_loc = cv2.minMaxLoc(result)
+
+  # 매칭 영역 좌표
   top_left = max_loc
   h, w = gray_template.shape
-  return display[top_left[1]:top_left[1]+h, top_left[0]:top_left[0]+w]
+  bottom_right = (top_left[0] + w, top_left[1] + h)
+
+  # 매칭 영역 추출
+  cropped = display[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
+  return cropped
 
 def get_cores(table, template):
   cores = []
+
   gray_table = cv2.cvtColor(table, cv2.COLOR_BGR2GRAY)
   gray_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+  # 윤곽선 검출
   blurred = cv2.GaussianBlur(table, (5, 5), 0)
-  edges = cv2.Canny(blurred, 90, 180)
+  edges = cv2.Canny(blurred, threshold1=90, threshold2=180)
   contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
   for cnt in contours:
-    if cv2.contourArea(cnt) < 500:
+    # 소형 객체 필터링
+    area = cv2.contourArea(cnt)
+    if area < 500:
       continue
-    approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
+
+    # 윤곽선 근사화
+    epsilon = 0.02 * cv2.arcLength(cnt, True)
+    approx = cv2.approxPolyDP(cnt, epsilon, True)
+
+    # 5 ~ 9각형 체크
     if 5 <= len(approx) <= 9:
       x, y, w, h = cv2.boundingRect(approx)
-      if 0.7 < (w / h) < 1.3:
+      aspect_ratio = w / h
+      # 가로/세로 비율 체크
+      if 0.7 < aspect_ratio < 1.3:
+        # 빈 코어 템플릿 매칭
         cropped = gray_table[y:y+h, x:x+w]
-        resized_template = cv2.resize(gray_template, (w, h))
-        result = cv2.matchTemplate(cropped, resized_template, cv2.TM_CCOEFF_NORMED)
+        h, w = cropped.shape
+        gray_template = cv2.resize(gray_template, (w, h))
+        result = cv2.matchTemplate(cropped, gray_template, cv2.TM_CCOEFF_NORMED)
         _, max_val, _, _ = cv2.minMaxLoc(result)
+
+        # 코어 추출
         if max_val < 0.5:
           cores.append(table[y:y+h, x:x+w])
   return cores
 
 def get_enhanced_cores(cores):
   enhanced_cores = []
+
   dark_color = np.array([0, 0, 0])
   light_color = np.array([94, 115, 113])
+
   for c in cores:
+    # 강화 코어 색 마스킹
     mask = cv2.inRange(c, dark_color, light_color)
+
+    # 코어 배경 좌표
     h, w = mask.shape
     x = w // 6
     y = (h // 2) - 5
-    white, black = 0, 0
+
+    # 코어 배경 분석
+    white = 0
+    black = 0
     for i in range(10):
       if mask[y+i, x] == 255:
         white += 1
       else:
         black += 1
+
+    # 마스킹 체크
     if white > black:
       enhanced_cores.append(c)
   return enhanced_cores
 
 def get_skills(cores):
   skills = []
-  for c in cores:
+  for idx, c in enumerate(cores):
     hsv = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, np.array([0, 0, 0]), np.array([0, 0, 0]))
+    color = np.array([0, 0, 0])
+
+    # 스킬 테두리 마스킹
+    mask = cv2.inRange(hsv, color, color)
+
+    # 윤곽선 검출
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 무효 객체 필터링
     if not contours:
       continue
+
+    # 대형 객체 좌표
     biggest = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(biggest)
-    skills.append(c[y:y+h, x:x+w])
+
+    # 스킬 추출
+    cropped = c[y:y+h, x:x+w]
+    skills.append(cropped)
+
   return skills
 
-def parse_skills(core_skills, job_folder):
-  def apply_border(image):
-    color = [221, 221, 204]
-    white = [255, 255, 255]
-    for x in range(1, 31):
-      image[1, x] = color
-      image[30, x] = color
-      image[2, x] = white
-      image[29, x] = white
-    for y in range(2, 30):
-      image[y, 1] = color
-      image[y, 30] = color
-      image[y, 2] = white
-      image[y, 29] = white
-    return image
-
+def parse_skills(core_skills, job):
   def generate_skills(files):
+    def apply_border(image):
+      color = [221, 221, 204]
+      white = [255, 255, 255]
+      for x in range(1, 31):
+        image[1, x] = color
+        image[30, x] = color
+        image[2, x] = white
+        image[29, x] = white
+      for y in range(2, 30):
+        image[y, 1] = color
+        image[y, 30] = color
+        image[y, 2] = white
+        image[y, 29] = white
+      return image
+
     skills = []
     size = 32
-    images, names = zip(*files)
-    resized = [cv2.resize(img, (size, size)) for img in images]
-    files = list(zip(resized, names))
+
+    images, file_names = zip(*files)
+    resized_images = []
+    for image in images:
+      image = cv2.resize(image, (size, size))
+      resized_images.append(image)
+    files = list(zip(resized_images, file_names))
+
+    # 삼각형 마스크 생성
     triangle_mask = np.zeros((size, size), dtype=np.uint8)
-    pts = np.array([[0, 0], [31, 0], [16, 16]], np.int32).reshape((-1, 1, 2))
+    pts = np.array([[0, 0], [31, 0], [16 ,16]], np.int32)
+    pts = pts.reshape((-1, 1, 2))
     cv2.fillPoly(triangle_mask, [pts], 255)
-    for combo in permutations(files, 3):
-      imgs, names = zip(*combo)
-      canvas = np.zeros_like(imgs[0])
-      for c in range(3):
-        canvas[:, :, c] = np.where(triangle_mask == 255, imgs[1][:, :, c], canvas[:, :, c])
+
+    combination_files = permutations(files, 3)
+    for combo_file in combination_files:
+      combo_images, combo_file_names = zip(*combo_file)
+      canvas = np.zeros_like(combo_images[0])
+
+      # 중앙 이미지 배치
+      for center in range(3):
+        canvas[:, :, center] = np.where(triangle_mask == 255, combo_images[1][:, :, center], canvas[:, :, center])
+
+      # 좌우 이미지 배치
       for y in range(size):
         for x in range(size):
           if triangle_mask[y, x] == 0:
-            canvas[y, x] = imgs[0][y, x] if x < 16 else imgs[2][y, x]
-      skills.append((apply_border(canvas), names))
+            if x < 16:
+              canvas[y, x] = combo_images[0][y, x]
+            else:
+              canvas[y, x] = combo_images[2][y, x]
+      canvas = apply_border(canvas)
+      skills.append((canvas, combo_file_names))
     return skills
 
-  def find_matching(core, generated_images):
-    core = cv2.resize(core, (32, 32))
-    scores = [cv2.matchTemplate(core, cv2.resize(g, (32, 32)), cv2.TM_CCOEFF_NORMED)[1] for g in generated_images]
-    return np.argmax(scores)
+  def find_matching_skill(core_skill, generated_skills):
+    size = 32
+    core_skill = cv2.resize(core_skill, (size, size))
 
-  files = [(cv2.imread(os.path.join(job_folder, f)), f[:-4]) for f in os.listdir(job_folder) if f.endswith('.png')]
-  generated = generate_skills(files)
-  gen_images, _ = zip(*generated)
-  return [generated[find_matching(skill, gen_images)] for skill in core_skills]
+    vals = []
+    for generated_skill in generated_skills:
+      generated_skill = cv2.resize(generated_skill, (size, size))
+
+      # 스킬 템플릿 매칭
+      result = cv2.matchTemplate(core_skill, generated_skill, cv2.TM_CCOEFF_NORMED)
+      _, max_val, _, _ = cv2.minMaxLoc(result)
+
+      vals.append(max_val)
+    most_val_index = vals.index(max(vals))
+    return most_val_index
+
+  # 직업 스킬 수집
+  files = []
+  for file_name in os.listdir(job):
+    if file_name.lower().endswith('.png'):
+      path = os.path.join(job, file_name)
+      image = cv2.imread(path)
+      files.append((image, file_name[:-4]))
+
+  # 스킬 조합 생성
+  generated_skills = generate_skills(files)
+  generated_skill_images, _ = zip(*generated_skills)
+
+  # 스킬 분석
+  parsed_skills = []
+  for core_skill in core_skills:
+    skill_index = find_matching_skill(core_skill, generated_skill_images)
+    parsed_skills.append(generated_skills[skill_index])
+
+  return parsed_skills
 
 def find_combinations(core_skills, target_skills):
+  def get_same_skill_count_avg(main_skill_indices, min_len, combo):
+    same_skill_total_count = 0
+    for i in range(min_len):
+      main_skill = core_skills[combo[i]][0]
+      same_skill_count = len(main_skill_indices[main_skill])
+      same_skill_total_count += same_skill_count
+    return same_skill_total_count / min_len
+
   n = len(target_skills)
-  skill_index = defaultdict(list)
+
+  # 메인 스킬 인덱싱
+  main_skill_indices = defaultdict(list)
   for i, core_skill in enumerate(core_skills):
-    skill_index[core_skill[0]].append(i)
-  all_main = list(skill_index.keys())
-  min_case = math.ceil(2 * n / 3)
-  max_case = min(2 * n, len(all_main))
-  valid_combos = []
+    main_skill_indices[core_skill[0]].append(i)
+
+  all_main_skills = list(main_skill_indices.keys())
+  min_case_count = math.ceil(2 * n / 3)
+  max_case_count = min(2 * n, len(all_main_skills)) + 1
+
+  valid_combinations = []
   min_len = float('inf')
-  for cnt in range(min_case, max_case + 1):
-    for main_combo in combinations(all_main, cnt):
-      idx_combos = [[]]
+
+  # 조합 생성
+  for cnt in range(min_case_count, max_case_count):
+    for main_combo in combinations(all_main_skills, cnt):
+      index_combinations = [[]]
       for skill in main_combo:
-        idx_combos = [prev + [i] for prev in idx_combos for i in skill_index[skill]]
-      for combo in idx_combos:
-        counter = Counter()
-        for i in combo:
-          counter.update(core_skills[i])
-        if all(counter[ts] >= 2 for ts in target_skills):
+        new_combos = []
+        for prev in index_combinations:
+          for idx in main_skill_indices[skill]:
+            new_combos.append(prev + [idx])
+        index_combinations = new_combos
+
+      # 조건 기반 조합 추출
+      for combo in index_combinations:
+        skill_counter = Counter()
+        for idx in combo:
+          skill_counter.update(core_skills[idx])
+        if all(skill_counter[skill] >= 2 for skill in target_skills):
           if len(combo) < min_len:
             min_len = len(combo)
-            valid_combos = [combo]
+            same_skill_avg = get_same_skill_count_avg(main_skill_indices, min_len, combo)
+            valid_combinations = [(combo, same_skill_avg)]
           elif len(combo) == min_len:
-            valid_combos.append(combo)
-  return valid_combos
+            same_skill_avg = get_same_skill_count_avg(main_skill_indices, min_len, combo)
+            valid_combinations.append((combo, same_skill_avg))
+          else:
+            break
+
+  final_combinations = []
+  max_avg = float(0)
+  for valid_combo in valid_combinations:
+    combination = valid_combo[0]
+    avg = valid_combo[1]
+    if avg > max_avg:
+      max_avg = avg
+      final_combinations = [core_skills[combo] for combo in combination]
+  return final_combinations
